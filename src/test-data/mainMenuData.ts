17b81c0cd29c7b9e899d3bc3bf85a1fa25623bf6
dakof <yordan.nedelchev94@gmail.com>
2025-11-29 14:19:17 +0200

    enriched main menu test data

---
+++ src/test-data/mainMenuData.ts
@@ -0,0 +1,36 @@
+// Trade Sub categories
+export const mainCategoriesList: string[] = [
+    'Dashboard',
+    'Markets',
+    'Trade',
+    'Features',
+    'About Us',
+    'Support']
+
+export const tradeSubCategoriesList: string[] = [
+    'Spot',
+    'Derivatives',
+    'Instant Buy',
+    'Panic Sell',
+    'Convert']
+
+export const tradeSubCategoriesDescriptionList: string[] = [
+    'Trade crypto with advanced tools',
+    'Trade USDT based derivatives',
+    'Buy Crypto Instantly',
+    'Sell Assets Quickly',
+    'Quickly swap any assets'
+]
+
+// Features Sub categories
+export const featuresSubCategoriesList: string[] = [
+    'Spot Exchange',
+    'Institutional',
+    'Buy & Sell'
+]
+
+export const featuresSubCategoriesDescriptionList: string[] = [
+    'Trade digital currencies',
+    'Access the full institutional package',
+    'The fastest way to buy crypto'
+]

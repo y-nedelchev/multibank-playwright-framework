3023d78d849275bc642b422d324256a49820f49a
dakof <yordan.nedelchev94@gmail.com>
2025-12-02 12:20:26 +0200

    added additional verification for TC08 - expect support chat is visible

--- tests/regression.spec.ts
+++ tests/regression.spec.ts
@@ -113,7 +113,7 @@ test.describe.parallel('Content Validation', () => {
       await pm.onHomePage().switchToNextMarketingBanner(banner.locator)
       expect(bannerText).toEqual(banner.expectedText)
       await pm.onHomePage().visitBannerRedirectionUrl(banner.locator, banner.expectedRedirectionText)
-      if(banner.expectedUrlPart.includes('24')){
+      if(banner.expectedText.includes('24')){
         await expect(pm.onHomePage().supportChatHeader).toBeVisible()
       }
       await expect(page).toHaveURL(new RegExp(banner.expectedUrlPart))
